effa32ca6a9ed07794b06783e986da2eb7d9de24
ShikQ <105293353+ShikQ@users.noreply.github.com>
2022-11-16 08:31:57 +0100

    Update spawner.js

---
+++ spawner.js
@@ -0,0 +1,13 @@
+class Spawner {
+    constructor(){
+        this.count = 0;
+    }
+
+    Spawner(){
+        while (this.count >= 2){
+            ball = new ball(x,y,d)
+            this.count = this.count + 1;
+        
+        }
+    }
+}
