2be21d9773d8678df52f863969e9a5fa5658f941
Donought <alfred.baly@gmail.com>
2022-11-16 08:23:08 +0100

    Weird tab change
    Idk why it's different on this pc

--- sketch.js
+++ sketch.js
@@ -1,27 +1,27 @@
 let balls = [];
 
 function setup() {
-  createCanvas(400, 400);
-  balls.push(new Ball((width / 4) * 1, height / 2, 50));
-  balls.push(new Ball((width / 4) * 3, height / 2, 50));
+	createCanvas(400, 400);
+	balls.push(new Ball((width / 4) * 1, height / 2, 50));
+	balls.push(new Ball((width / 4) * 3, height / 2, 50));
 }
 
 function draw() {
-  background(220);
+	background(220);
 
-  // Before running all these, set a variable to the current amount of balls in the array, so that it doesn't increase while they're running
-  let currentBalls = balls.length;
+	// Before running all these, set a variable to the current amount of balls in the array, so that it doesn't increase while they're running
+	let currentBalls = balls.length;
 
-  balls.forEach((b, i) => {
-    b.update();
-    b.wallCheck();
-    for (let j = i + 1; j < currentBalls; j++) {
-      b.ballCheck(balls[j]);
-    }
-    b.display();
-  });
+	balls.forEach((b, i) => {
+		b.update();
+		b.wallCheck();
+		for (let j = i + 1; j < currentBalls; j++) {
+			b.ballCheck(balls[j]);
+		}
+		b.display();
+	});
 }
 
 function keyPressed() {
-  balls.push(new Ball(width / 2, 25, 50));
+	balls.push(new Ball(width / 2, 25, 50));
 }
